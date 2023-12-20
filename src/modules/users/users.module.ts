0427af4e91cb3f90770ceca1746b9b1f1058a550
nghia0111 <20521659@gm.uit.edu.vn>
2023-12-21 05:00:03 +0700

    add get list user api

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,9 +1,16 @@
-// import { Module } from '@nestjs/common';
-// import { UsersService } from './users.service';
-// import { UsersController } from './users.controller';
+import { Module } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { User, UserSchema } from 'src/schemas/users.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 
-// @Module({
-//   controllers: [UsersController],
-//   providers: [UsersService],
-// })
-// export class UsersModule {}
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+    ]),
+  ],
+  controllers: [UsersController],
+  providers: [UsersService],
+})
+export class UsersModule {}
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,26 +1,14 @@
-// import { Injectable } from '@nestjs/common';
-// import { CreateUserDto } from './dto/create-user.dto';
-// import { UpdateUserDto } from './dto/update-user.dto';
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { UserRole } from 'src/common/constants';
+import { User } from 'src/schemas/users.schema';
 
-// @Injectable()
-// export class UsersService {
-//   create(createUserDto: CreateUserDto) {
-//     return 'This action adds a new user';
-//   }
+@Injectable()
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
-//   findAll() {
-//     return `This action returns all users`;
-//   }
-
-//   findOne(id: number) {
-//     return `This action returns a #${id} user`;
-//   }
-
-//   update(id: number, updateUserDto: UpdateUserDto) {
-//     return `This action updates a #${id} user`;
-//   }
-
-//   remove(id: number) {
-//     return `This action removes a #${id} user`;
-//   }
-// }
+  async findAll() {
+    return await this.userModel.find({role: UserRole.USER}).select('displayName avatar email');
+  }
+}
