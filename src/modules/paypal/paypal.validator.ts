59419602f28d8598a9b11b83ce35808aa6003ce9
nghia0111 <20521659@gm.uit.edu.vn>
2023-12-24 20:19:44 +0700

    Update paypal.validator.ts

--- src/modules/paypal/paypal.validator.ts
+++ src/modules/paypal/paypal.validator.ts
@@ -2,8 +2,7 @@ import * as Joi from 'joi';
 import { ValidationErrorMessages } from 'src/common/constants';
 
 export const DepositValidator = Joi.object({
-  amount: Joi.number().integer().min(1).messages({
-    'number.integer': ValidationErrorMessages.AMOUNT_INVALID,
+  amount: Joi.number().min(1).messages({
     'number.min': ValidationErrorMessages.AMOUNT_MIN,
   }),
 });
